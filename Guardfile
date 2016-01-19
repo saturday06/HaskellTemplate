c1c9aab7e3da12c16191140e8e3cad473590ec3a
Isamu Mogi <isamu@leafytree.jp>
2016-01-19 20:28:19 +0900

    x

--- Guardfile
+++
@@ -1,8 +0,0 @@
-notification :off
-
-guard :haskell do
-  watch(%r{test/.+\.l?hs$})
-  watch(%r{.+\.l?hs$})
-  watch(%r{\.cabal$})
-  callback(:start_begin) { `cabal configure --enable-tests` }
-end
--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,17 @@
-module Main where
+#!/usr/bin/env runhaskell
+{-# LANGUAGE OverloadedStrings #-}
 
-main = print "Hello"
+import Network.HTTP.Client
+import Network.HTTP.Client.MultipartFormData
+import Network.HTTP.Types.Status (statusCode)
+import System.Environment
+import Data.Function
+
+main = do
+  manager <- newManager defaultManagerSettings
+  path <- getArgs & fmap (!! 0)
+  url <- parseUrl "http://pyazo.office/upload.cgi"
+  request <- url { method = "POST" } & formDataBody [partBS "id" "", partFileSource "imagedata" path]
+  response <- httpLbs request manager
+  print $ "The status code was: " ++ (response & responseStatus & statusCode & show)
+  print $ responseBody response
